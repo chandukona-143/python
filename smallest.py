297a4f6562f3c242cf97dd7f35b6f66cecba8402
chandukona-143 <chandukona61@gmail.com>
2025-03-15 11:08:31 +0530

    Create smallest.py

---
+++ smallest.py
@@ -0,0 +1,16 @@
+a= int(input())
+b=int(input())
+c=int(input())
+if (a<b and a<c):
+    print(a)
+    
+elif(b<a and b<C):
+    print(b)
+elif(c<a and c<b):
+    print(c)
+elif(a==b and b==c):
+    print(a)
+elif(a==b and b<c):
+    print(a)
+  
+    
