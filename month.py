c1bd6905cebda67fd1e1347a94a7fbe8b29e0603
chandukona-143 <chandukona61@gmail.com>
2025-03-15 11:09:15 +0530

    Create month.py

---
+++ month.py
@@ -0,0 +1,9 @@
+month =int(input())
+if(month ==1 or month==2 or month==12):
+    print("winter")
+elif(month==3 or month==4 or month==5):
+    print("summer")
+elif(month==6 or month==7 or month==8):
+    print("rainy")
+elif(month==9 or month==10 or month==11):
+    print("monsoon")
